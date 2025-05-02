235e9f5bda4a714f0299a0b5466e831f647710fe
vxsk <felipe_adomi@hotmail.com>
2025-05-02 16:03:39 -0300

    Adiçao da seçao FAQ

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     
     for (let i = 0; i < buttons.lenght; i++) {
@@ -13,6 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 })
+
+for (let i = 0; i < questions.lenght; i++) {
+    questions[i].addEventListener('click', abreOuFechaResposta);
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
