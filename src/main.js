0b4f0d2e4ce50ad3ad462b48c302d7c7f980dace
vxsk <felipe_adomi@hotmail.com>
2025-05-09 11:27:21 -0300

    Adiçao da Header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
-    
-    for (let i = 0; i < buttons.lenght; i++) {
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();  
+        } else {
+            exibeElementosHeader();
+        }
+    });
+
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
@@ -11,12 +22,22 @@ document.addEventListener('DOMContentLoaded', function() {
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
-        })
+        });
     }
-})
 
-for (let i = 0; i < questions.lenght; i++) {
-    questions[i].addEventListener('click', abreOuFechaResposta);
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+});
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
 
 function abreOuFechaResposta(elemento) {
@@ -29,7 +50,7 @@ function abreOuFechaResposta(elemento) {
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for (let i = 0; i < buttons.lenght; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
